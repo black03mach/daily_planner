adab08fb796a96edf623133b72f93e031cd199eb
Patrick Wells <pat.taylorwells@gmail.com>
2020-07-07 22:13:38 -0400

    final

--- script.js
+++ script.js
@@ -1,50 +1,66 @@
 var dailyTasks = []
 var localStorage = JSON.parse(localStorage.getItem('inputs'))
-var currentHour = parseInt(moment().format("HH"));
+var currentHour = parseInt(moment().format("HH")) - 8;
 var dailyHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 var timeSlotId = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"];
 var militaryTime = ["08:00:00", "09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00"];
 var saveButton = document.querySelector('#saveButton');
-console.log(currentHour);
-console.log(dailyHours)
 
 // Display current time & create a dynamic variable
-var displayCurrentTime = function() {
+var displayCurrentTime = function () {
     document.getElementById("datetime")
-    .innerHTML = moment().format('MMMM Do YYYY, HH:mm:ss a');
+        .innerHTML = moment().format('MMMM Do YYYY, HH:mm:ss a');
 }
 setInterval(displayCurrentTime, 1000);
 
 // Save button
-//saveButton.addEventListener("click", saveActivity);
+$('.saveButton').on('click', saveActivity)
 
-function saveActivity(){
-    var inputObj = {
+function saveActivity(event) {
 
+    for (var j = 0; j < timeSlotId.length; j++) {
+        var helpfulMath = j + 8
+        var inputVal = $('#' + helpfulMath).val();
+        console.log($('#' + helpfulMath).val())
+        localStorage.setItem(helpfulMath + "", JSON.stringify(inputVal));
     }
 }
 
 // Render Color, use an if statement based on current hour compared to daily hours.
-for (var i = 0 ; i < timeSlotId.length ; i++) {
+for (var i = 0; i < timeSlotId.length; i++) {
+    //console.log(currentHour)
+    // console.log(dailyHours[i])
+    // console.log((currentHour == dailyHours[i]))
+    // console.log(currentHour < dailyHours[i]);
+    // console.log(i)
+    var helpfulMath = i + 8
     // IF the moment.js time is equal to the daily hour, color the background orange and make the input unactionable.
-    if (currentHour === dailyHours[i]) {
-        $('#inputForm').addClass('now');
-        $('#inputForm').prop('disabled', true);
+    if (currentHour == dailyHours[i]) {
+        $('#' + helpfulMath).addClass('now');
+        $('#' + helpfulMath).prop('disabled', true);
     }
     // IF the moment.js time is less than the daily hour, color the background green
+
     else if (currentHour < dailyHours[i]) {
-        $('#inputForm').addClass('forward');
-        $('#inputForm').prop('enabled', true);
+        $('#' + helpfulMath).addClass('forward');
+        $('#' + helpfulMath).prop('enabled', true);
     }
     // IF the moment.js time is less than the daily hour, color the background gray and make the input inactionable.
     else {
-        $('#inputForm').addClass('before');
-        $('#inputForm').prop('disabled', true);
+        $('#' + helpfulMath).addClass('before');
+        $('#' + helpfulMath).prop('disabled', true);
     }
 }
 
 
 // Render saved cells
-function renderCells(){
-
+for (var k = 0; k < timeSlotId.length; k++) {
+    var helpfulMath = k + 8
+    var dataStored = (localStorage.getItem(helpfulMath + ""));
+    console.log(dataStored)
+    // this code leaves quotes.
+    //var newValue = dataStored.replace('"', '');
+    trimmedValue = dataStored.slice(1,-1);
+    $('#' + helpfulMath).val(trimmedValue);
 }
+
